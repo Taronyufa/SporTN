758a4cfbba8ebedc1b38d309234144f57fe7e9ff
milanitommaso <milani.tommaso2004@gmail.com>
2024-11-11 14:35:48 +0100

    clean files

--- app.js
+++ app.js
@@ -1,41 +1,26 @@
-var createError = require('http-errors');
+require('dotenv').config();
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+var util = require('util');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Serving static files
+app.use(express.static('public'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Routes
+app.get('/', function(req, res) {
+    res.send('This is the homepage');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// reservations route, using the reservations.js file
+var reservations = require('./routes/reservations');
+app.use('/reservations', reservations);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = app;
