16d2ebb631f1cd4eb0947e0398b0de9904329a0d
milanitommaso <milani.tommaso2004@gmail.com>
2024-12-02 19:02:10 +0100

    moved mongodb url to .env

--- routes/connection.js
+++ routes/connection.js
@@ -1,6 +1,9 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:ABC_123456@sportn.pbwkv.mongodb.net/?retryWrites=true&w=majority&appName=SporTN";
+
+// get uri from .env file
+require('dotenv').config();
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
