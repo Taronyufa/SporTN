1aec0a24a6ad26c3714987187ca04c5d5b740dcf
milanitommaso <milani.tommaso2004@gmail.com>
2024-12-27 22:39:24 +0100

    fix image dimension on public event view

--- frontend/src/views/PublicEventView.vue
+++ frontend/src/views/PublicEventView.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="container mx-auto p-6">
         <div v-if="event" class="bg-white shadow-md rounded-lg p-6">
-            <!-- Event Image -->
-            <img
-            :src="event.foto_url"
-            :alt="event.nome"
-            class="w-full h-64 object-cover rounded-lg mb-6"
-            />
-    
             <!-- Event Details -->
             <h1 class="text-3xl font-bold mb-4">{{ event.nome }}</h1>
             <p class="text-gray-600 mb-2"><strong>Data e Ora Inizio:</strong> {{ event.data_inizio }}</p>
             <p class="text-gray-600 mb-2"><strong>Data e Ora Fine:</strong> {{ event.data_fine }}</p>
             <p class="text-gray-600 mb-2"><strong>Posizione:</strong> {{ event.posizione }}</p>
             <p class="text-gray-600 mb-2"><strong>Description:</strong> {{ event.descrizione }}</p>
+            
+            <!-- Event Image -->
+            <img
+            :src="event.foto_url"
+            :alt="event.nome"
+            class="w-full object-cover rounded-lg mb-6"
+            />
         </div>
     </div>
 </template>
