1225bdc87b5790bb38f4f267953494830f4b666b
milanitommaso <milani.tommaso2004@gmail.com>
2024-12-27 17:11:05 +0100

    modified routing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,24 +7,24 @@ import MakeReview from '../views/MakeReviewView.vue';
 import MakeReport from '../views/MakeReportView.vue';
 import Fields from '../views/FieldsView.vue';
 import PublicEvents from '../views/PublicEventsView.vue';
+import PublicEvent from '../views/PublicEventView.vue';
 import Login from '../views/LoginView.vue';
 import Register from '../views/RegisterView.vue';
 import Profile from '../views/ProfileView.vue';
-import EditProfile from '../views/EditProfileView.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/my-bookings', name: 'MyBookings', component: MyBookings },
   { path: '/bookings/:id', name: 'Booking', component: Booking },
-  { path: '/fields/:id/make-review', name: 'MakeBooking', component: MakeBooking },
-  { path: '/fields/:id/make-report', name: 'MakeReview', component: MakeReview },
-  { path: '/make-report/:id', name: 'MakeReport', component: MakeReport },
+  { path: '/book-field/:id', name: 'MakeBooking', component: MakeBooking },
+  { path: '/fields/:id/review', name: 'MakeReview', component: MakeReview },
+  { path: '/fields/:id/report', name: 'MakeReport', component: MakeReport },
   { path: '/fields', name: 'Fields', component: Fields },
   { path: '/public-events', name: 'PublicEvents', component: PublicEvents },
+  { path: '/public-events/:id', name: 'PublicEvent', component: PublicEvent },
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
   { path: '/profile', name: 'Profile', component: Profile },
-  { path: '/edit-profile', name: 'EditProfile', component: EditProfile },
 ];
 
 const router = createRouter({
